a6b4556c36610b9d1a476aaa7510cfb0a9a580c0
Steven Ng <szeyungng@gmail.com>
2023-10-17 16:11:24 -0600

    Moving functions definition to c files

--- ENCM511_DriverProject_3.X/io.c
+++ ENCM511_DriverProject_3.X/io.c
@@ -8,9 +8,102 @@
 
 #include "xc.h"
 
-void IOinit()
-{
-    AD1PCFG = 0xFFFF;
+void IOinit() {
+   
+    // SET up INPUT Ports
+    TRISAbits.TRISA2 = 1;
+    TRISAbits.TRISA4 = 1;
+    TRISBbits.TRISB4 = 1;
+    
+    // SET up OUTPUT Ports
+    TRISBbits.TRISB8 = 0;
+        
+    // Enable Input Change Notification Interrupt
+    IEC1bits.CNIE = 1;
+    
+    // Enable individual interrupt pins
+    CNEN2bits.CN30IE = 1;
+    CNEN1bits.CN0IE  = 1;
+    CNEN1bits.CN1IE  = 1;
+    
+    // Set Input Change Notification Flag to 0
+    IFS1bits.CNIF = 0;
+    
+    // Select digital on pin 7
+    AD1PCFGbits.PCFG4 = 1;
+    
+    // Set weak pull up for inputs
+    CNPU2bits.CN30PUE = 1;
+    CNPU1bits.CN0PUE  = 1;
+    CNPU1bits.CN1PUE  = 1;
 }
-void IOcheck();
 
+uint8_t IOcheck() {
+    
+    uint8_t PB1 = !(PORTAbits.RA2);
+    uint8_t PB2 = !(PORTAbits.RA4);
+    uint8_t PB3 = !(PORTBbits.RB4);
+    
+    if ((PB1 && !PB2 && !PB3) || (!PB1 && PB2 && !PB3) || (!PB1 && !PB2 && PB3)) {
+        
+        // If only one of three buttons is actively pressed...
+        if(PB1)
+            return 0;
+        else if(PB2)
+            return 1;
+        else if(PB3)
+            return 2;
+        
+    } else if (!(PB1 || PB2 || PB3)) {
+        
+        // If no buttons are actively pressed...
+        return 3;
+        
+    } else {
+        
+        // More than one buttons are actively pressed
+        return 4;
+        
+    }
+}
+
+void setStateConditions(uint8_t state) {
+    
+    // For states 0-2 (dynamic states), reset LATB8 because
+    // the LED will toggle back on once arriving back in main()
+    
+    // For states 3 and 4 (static states), set/reset the LED. 
+    // LED will not be toggled again until entering a dynamic state
+    
+    switch(state)
+    {
+        case 0:
+            LATBbits.LATB8 = 0;
+            break;
+            
+        case 1:
+            LATBbits.LATB8 = 0;
+            break;
+            
+        case 2:
+            LATBbits.LATB8 = 0;
+            break;
+
+        case 3:
+            LATBbits.LATB8 = 0;
+            break;
+            
+        case 4:
+            LATBbits.LATB8 = 1;
+            break;
+            
+    }
+    
+    counter = 0;
+}
+
+void toggleLED() {
+    // ~ operator complements the bit
+    
+    LATBbits.LATB8 = ~LATBbits.LATB8;
+}
--- ENCM511_DriverProject_3.X/timer.c
+++ ENCM511_DriverProject_3.X/timer.c
@@ -8,5 +8,43 @@
 
 #include "xc.h"
 
-void delay_ms(uint16_t time_ms);
+void (uint16_t time_ms);
+void customWait(uint8_t state) {
+    
+    // Set the counter limit per the current state
+    // counter_lim = 50000 produces ~0.5 second delay
+    // counter_lim = 100000 produces ~1 second delay
+    // counter_lim = 200000 produces ~2 second delay
+    // States 3 and 4 are static, therefore set counter_lim to 0
+
+    switch(state)
+    {
+        case 0:
+            counter_lim = 50000;
+            break;
+            
+        case 1:
+            counter_lim = 100000;
+            break;
+            
+        case 2:
+            counter_lim = 200000;
+            break;
+            
+        default:
+            counter_lim = 0;
+            break;
+    }
+    
+    
+    // Reset counter to counter_lim, then count down to 0 to produce delay
+    
+    counter = counter_lim;
+    
+    while(counter > 0) {
+        counter--;
+    }
+
+}
+
 void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void);
