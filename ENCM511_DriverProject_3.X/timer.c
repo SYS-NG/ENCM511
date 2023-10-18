79af54a6971520d651c6777aacda37a24e92133f
Steven Ng <szeyungng@gmail.com>
2023-10-17 20:25:42 -0600

    Added comments to main.c and timer.c, removed extra comments in io.h

--- ENCM511_DriverProject_3.X/timer.c
+++ ENCM511_DriverProject_3.X/timer.c
@@ -7,37 +7,44 @@
 
 #include "xc.h"
 
+// Configure Timer Registers
 void timerInit()
 {
-    IEC0bits.T2IE = 1;
-    IFS0bits.T2IF = 0;
-    T2CONbits.T32 = 0;
-    
-    T2CONbits.TON = 0;
-    PR2           = 0;
-    TMR2          = 0;
+    IEC0bits.T2IE = 1; // Enable Timer2 Interrupt
+    IFS0bits.T2IF = 0; // Lower Timer2 Interrupt Status Flag
+    T2CONbits.T32 = 0; // Set Timer2 to be 16 bit
+    T2CONbits.TON = 0; // Turn off timer2
+    PR2           = 0; // Set Timer2 Period to 0
+    TMR2          = 0; // Set TMR2 to 0;
 }
 
+// delay function
 void delay_ms(uint16_t time_ms)
 {
+    // Based on millisecond to wait for, set Timer2 period accordingly
     switch(time_ms)
     {
+        // 1ms
         case 1:
             PR2 = 4;
             break;
-            
+        
+        // 500ms
         case 500:
             PR2 = 1953;
             break;
-            
+        
+        // 1000ms
         case 1000:
             PR2 = 3906;
             break;
         
+        // 5000ms    
         case 5000:
             PR2 = 19531;
             break;
-            
+        
+        // All other cases PR2 is 0
         default:
             PR2 = 0;
             break;
